714a4c7657941118a3ba4285b691bb6eff13c99c
Will_Weng <Will_Weng@cmoney.com.tw>
2022-08-01 20:36:28 +0800

    測試外部提供文字給sdk使用

--- Sources/MyLibraryDemo/SwiftUIView.swift
+++ Sources/MyLibraryDemo/SwiftUIView.swift
@@ -10,10 +10,15 @@ import Foundation
 @available(iOS 13.0, *)
 public class MyView {
     
-    public init() {
-        
+    public let host: String
+    
+    public init(host: String) {
+        self.host = host
     }
-    public func getTextString() -> String {
-        return "我是可以顯示的喔"
+    
+    public func getHost() {
+        print("測試 host", host)
     }
+    
 }
+
